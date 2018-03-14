3d870099e02077507f6523d3de6e30c30023e167
CecyGlezAre <37052999+CecyGlezAre@users.noreply.github.com>
2018-03-13 19:43:27 -0600

    Add files via upload

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BlogdOferta.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,13 @@ namespace BlogdOferta.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
-            return View();
+            var InfoOfertas = db.Database.SqlQuery<OfertasXCate>(@"Select B.OfertaID, B.CategoriaID, B.DescripcionOfer, B.ImagenNeg, B.Precio, B.Existencia, B.Porcentaje, B.Estado, B.FechaOferInicial, B.FechaOferFinal, A.NomCate
+                                                                    From Categorias A Inner Join Ofertas B On (A.CategoriaID = B.CategoriaID) order by FechaOferInicial Desc").Take(10).ToList();
+            return View(InfoOfertas);
         }
 
         public ActionResult About()
---
+++ Models/OfertasXCate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogdOferta.Models
+{
+    public class OfertasXCate
+    {
+        public int OfertaID { get; set; }
+
+        public int CategoriaID { get; set; }
+
+        public string DescripcionOfer { get; set; }
+
+        public string ImagenNeg { get; set; }
+
+        public double Precio { get; set; }
+
+        public int Existencia { get; set; }
+
+        public int Porcentaje { get; set; }
+
+        public int Estado { get; set; }
+
+        public DateTime FechaOferInicial { get; set; }
+
+        public DateTime FechaOferFinal { get; set; }
+
+        public string NomCate { get; set; }
+    }
+}
